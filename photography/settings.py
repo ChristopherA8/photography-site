c37e9e54c687684f2d43917be78e4ead26468270
Chr1sDev <christopheranderson3@me.com>
2023-08-04 21:04:47 +0000

    migrate from sqlite3 to mysql

--- photography/settings.py
+++ photography/settings.py
@@ -84,10 +84,22 @@ WSGI_APPLICATION = 'photography.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'photography',
+        'USER': 'chris',
+        'PASSWORD': str(os.getenv('DB_PASSWORD')),
+        'HOST':'localhost',
+        'PORT':'3306',
     }
 }
 
