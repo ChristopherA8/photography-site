7a35c72792e1e7b7e649c547a565f43c2bf52de3
Chr1sDev <christopheranderson3@me.com>
2023-08-14 19:54:27 +0000

    add basic user auth functionality

--- photography/settings.py
+++ photography/settings.py
@@ -145,3 +145,7 @@ STATIC_URL = '/photography/static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+LOGIN_REDIRECT_URL = "/photography"
+LOGOUT_REDIRECT_URL = "/photography"
--- photography/urls.py
+++ photography/urls.py
@@ -21,7 +21,8 @@ from django.conf import settings
 
 urlpatterns = [
     path('photography/admin/', admin.site.urls),
-    path('photography/', include('portfolio.urls'))
+    path('photography/', include('portfolio.urls')),
+    path("photography/accounts/", include("django.contrib.auth.urls")),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
